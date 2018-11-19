132ba5e7366d515e8dee58989abc4b5fe5b0bd91
emendezduran <emendezduran@nautilus25.danielcastelao.org>
2018-11-19 12:40:39 +0100

    commit

--- prog_boletin_9/Boletin_9_6.java
+++ prog_boletin_9/Boletin_9_6.java
@@ -1,9 +1,28 @@
 
 package prog_boletin_9;
 
-
+import java.util.Scanner;
 
 /**
  *
  * @author emendezduran
  */
+public class Boletin_9_6 {
+    
+public static void main(String[] args) {
+
+int contTrabajadores = 0;
+int contSueldo999 = 0;
+int contSueldo1750 = 0;
+
+Scanner scan1 = new Scanner(System.in);
+
+System.out.println("Teclea el sueldo del trabajador" ++contTrabajadores);
+
+double sueldo = scan1.nextfloat;
+if(sueldo>=1000 && sueldo<=1750){
+++contSueldo1750;
+} else 
+
+}
+}
